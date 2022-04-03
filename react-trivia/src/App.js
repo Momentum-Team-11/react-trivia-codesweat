3bb6423a642d638c503b75c20e932a10a031e591
KarenElaine <karenelaine.stewart@yahoo.com>
2022-04-03 15:33:18 -0400

    added option to play again

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -5,19 +5,21 @@ import Question from "./Question";
 
 const App = () => {
   const categoriesURL = "https://opentdb.com/api_category.php";
-  const questionsURL = "https://opentdb.com/api.php?amount=10&type=boolean&category=";
+  const questionsURL =
+    "https://opentdb.com/api.php?amount=10&type=boolean&category=";
   const [categories, setCategories] = useState([]);
   const [selected, setSelected] = useState(null);
   const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0);
   const [endGame, setEndGame] = useState(false);
+  const [playAgain, setPlayAgain] = useState(false);
 
   // ask for a list of categories
   useEffect(() => {
     axios.get(categoriesURL).then((response) => {
       setCategories(response.data.trivia_categories);
     });
-  }, []);
+  }, [endGame]);
 
   // useEffect(() => {}, [])
 
@@ -30,9 +32,16 @@ const App = () => {
   }, [selected]);
 
   if (endGame) {
-    return `Well done! Your score is: ${score} out of ${questions.length}!`;
+    return (
+      <div>
+        <p>
+          Well done! Your score is: {score} out of {questions.length}!
+        </p>
+        <button onClick={() => {console.log('play again clicked!'); setEndGame(false); setPlayAgain(true); setQuestions([]); setScore(0)}}>Play Again</button>
+      </div>
+    );
   }
-
+  if (score === 0 && playAgain || endGame === false) {
   return (
     // Ternary operator syntax
     // condition ? what to do if true : what to do if false
@@ -63,7 +72,18 @@ const App = () => {
         })
       )}
     </div>
-  );
+  )};
+  return null
 };
+
+// if (playAgain) {
+//   console.log('play again clicked!')
+//     return (
+//       <div id="replay">
+//         <button onClick={() => setPlayAgain(true)}>Play Again</button>
+//         </div>
+// )};
+
+// );
 
 export default App;
